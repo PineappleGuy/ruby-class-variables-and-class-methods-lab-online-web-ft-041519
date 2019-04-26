3a1b618daf1fe55a2d286fbd0e14b0cd9f11c707
Garth Lilly <garff247@gmail.com>
2019-04-26 23:39:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,8 @@ class Song
   def self.genre_count
     hash = {}
     @@genres.each do |genre|
-
-        hash[genre] += 1
+if hash.include?(genre)
+        hash.push(genre: += 1)
     end
     return hash
   end
