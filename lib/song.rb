9adf39a0280fbd471ec48509c7111570dfc1d0fe
Garth Lilly <garff247@gmail.com>
2019-04-26 21:33:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,34 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
-
+  @@count = 0
+  @@artists = []
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+
   end
 
-  def name=(name)
-    @name = name
+  def self.count
+    @@song_count
   end
 
-  def name
-    @name
+  def self.artists
+
   end
 
+  def self.genres
+
+  end
+
+  def self.genre_count
+
+  end
+
+  def self.artist_count
+
+  end
+  
 end
